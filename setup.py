00e9c742e49d8cc553466954b4386e2a2246a607
larsheinen <https://meet.google.com/yqv-jgpo-cjr?hs=151>
2021-01-08 09:39:44 +0100

    changed pip project name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pygochook-Lars147", # Replace with your own username
+    name="pygochook", # Replace with your own username
     version="0.0.1",
     author="Lars Heinen",
     author_email="larsheinen@gmail.com",
