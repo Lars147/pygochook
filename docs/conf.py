b9e971ab19e1abb330fe32f9b726efb29a5caf80
larsheinen <https://meet.google.com/yqv-jgpo-cjr?hs=151>
2021-01-10 12:49:22 +0100

    no static and templates dirs as they are not used

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ napoleon_type_aliases = None
 napoleon_attr_annotations = True
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -66,6 +66,6 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 autoclass_content = 'both'
