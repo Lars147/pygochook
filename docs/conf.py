822c1de60537265e51b0c0f58eb2ba5c38a43a4f
larsheinen <https://meet.google.com/yqv-jgpo-cjr?hs=151>
2021-01-10 13:25:58 +0100

    add "sphinx_rtd_theme" to extensions

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ release = '0.0.2'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx_rtd_theme']
 
 # Napoleon settings
 napoleon_google_docstring = True
